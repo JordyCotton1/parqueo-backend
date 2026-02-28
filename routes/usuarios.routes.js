0d97d67a5b56eaa777b7c5d7a339cb69b0fb328c
Enrique Cottón <124799549+JordyCotton1@users.noreply.github.com>
2026-02-28 01:24:25 -0600

    Add files via upload

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -5,29 +5,28 @@ const {
   registrar,
   login,
   listarUsuarios,
-  crearInicial   // ✅ agregado
+  crearInicial   // 👈 AGREGADO
 } = require("../controllers/usuarios.controller");
 
 const { verificarToken, soloGerente } = require("../middlewares/auth");
 
-// ===============================
-// Crear usuario inicial (solo 1 vez)
-// ===============================
-router.get("/crear-inicial", crearInicial);
+// ================================
+// CREAR USUARIO INICIAL (Temporal)
+// ================================
+router.get("/crear-inicial", crearInicial);  // 👈 AGREGADO
 
-// ===============================
+// ================================
 // Login
-// ===============================
+// ================================
 router.post("/login", login);
 
-// ===============================
+// ================================
 // Crear usuario (solo gerente)
-// ===============================
+// ================================
 router.post("/", verificarToken, soloGerente, registrar);
 
-// ===============================
+// ================================
 // Listar usuarios
-// ===============================
+// ================================
 router.get("/", verificarToken, soloGerente, listarUsuarios);
-
 module.exports = router;
